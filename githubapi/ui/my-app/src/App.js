96148e1fea5ceaa94fa0764192ce9098276ba1f9
kanav89 <kanavg004@gmail.com>
2023-12-16 22:51:23 -0800

    made changes to the UI

--- githubapi/ui/my-app/src/App.js
+++ githubapi/ui/my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, TextInput, Progress } from 'flowbite-react';
 import "./App.css";
 
 function App() {
@@ -9,59 +10,73 @@ function App() {
   const [ed, setEd] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
- 
+  const [progress, setProgress] = useState(0);
+
+  const handleInputChange = () => {
+    setProgress((prevProgress) => prevProgress + 25);
+  };
 
   const handleSubmit = async () => {
-  console.log('Submitting...');
-  setLoading(true);
-  setError(null);
+    console.log('Submitting...');
+    setLoading(true);
+    setError(null);
+    setData([]);
+    setProgress(0);
 
-  const apiUrl = `/contributions?username=${name}&token=${token}&start_date=${sd}&end_date=${ed}`;
-  console.log('API URL:', apiUrl);
+    const apiUrl = `/contributions?username=${name}&token=${token}&start_date=${sd}&end_date=${ed}`;
+    console.log('API URL:', apiUrl);
 
-  try {
-    const res = await fetch(apiUrl);
-    console.log('Response:', res);
+    try {
+      const res = await fetch(apiUrl);
+      console.log('Response:', res);
 
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
+      if (!res.ok) {
+        alert(`HTTP error! Status: ${res.status}`);
+      }
+
+      const data = await res.json();
+      console.log('Data:', data);
+
+      setData(data);
+    } catch (error) {
+      setError(error.message);
+      console.error('Error fetching data:', error);
+      console.log('error');
+    } finally {
+      setLoading(false);
+      console.log('Finished submitting');
     }
-    const data = await res.json();
-    console.log('Data:', data);
-    
+  };
 
-    setData(data);
-  } catch (error) {
-    setError(error.message);
-    console.error('Error fetching data:', error);
-    console.log('error');
-  } finally {
-    setLoading(false);
-    console.log('Finished submitting');
-  }
-};
+  React.useEffect(() => {
+    document.title = 'Github!';
+  }, []);
 
+  return (
+    <>
+      <div className='full-page-container'>
+        <strong style={{ fontSize: "x-large" }}>Welcome to Github Contribution Tracker</strong>
+        <h1 style={{ fontSize: 'x-large' }}>Enter Information!</h1>
 
-return (
-  <>
-    <input type="text" name="name" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
-    <input type="text" name="token" placeholder="Enter token" value={token} onChange={(e) => setToken(e.target.value)} />
-    <input type="text" name="sd" placeholder="Enter sd" value={sd} onChange={(e) => setSd(e.target.value)} />
-    <input type="text" name="ed" placeholder="Enter ed" value={ed} onChange={(e) => setEd(e.target.value)} />
-    <button onClick={handleSubmit}>Submit</button>
-
-    {loading && <p>Loading...</p>}
-    {error && <p style={{ color: 'red' }}>{error}</p>}
-    {data.length > 0 && (
-      <div>
-        <h2>Data:</h2>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-        {}
+        <form className="flex max-w-md flex-col gap-4">
+          <TextInput className="i1" type="text" name="name" placeholder="Enter Name" value={name} onChange={(e) => { setName(e.target.value); handleInputChange(); }} />
+          <TextInput className="i2" type="text" name="token" placeholder="Enter token" value={token} onChange={(e) => { setToken(e.target.value); handleInputChange(); }} />
+          <TextInput className="i3" type="text" name="sd" placeholder="Enter start date(yyyy-mm-dd)" value={sd} onChange={(e) => { setSd(e.target.value); handleInputChange(); }} />
+          <TextInput className="i4" type="text" name="ed" placeholder="Enter end date(yyyy-mm-dd)" value={ed} onChange={(e) => { setEd(e.target.value); handleInputChange(); }} />
+          <Button gradientMonochrome="info" onClick={handleSubmit}>Submit</Button>
+          <Progress progress={progress} />
+          {loading && <p>Loading...</p>}
+          {error && <p style={{ color: 'red', fontSize: 'x-large', fontFamily: 'sans-serif' }}>{error}</p>}
+          {data.length > 0 && (
+            <div className="ouput">
+              <h2>Data:</h2>
+              <h2>{JSON.stringify(data, null, 2)}</h2>
+            </div>
+          )}
+        </form>
       </div>
-    )}
-  </>
-);
-
+    </>
+  );
 }
 
 export default App;
