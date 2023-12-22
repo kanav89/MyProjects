402eab187b830d88dda34a954a1f76e6d06c8d49
kanav89 <kanavg004@gmail.com>
2023-12-22 11:57:09 -0700

    portfolio

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navibar from "./components/Navibar";
+import About from "./components/About"
+import Skills from "./components/Skills"
+
+
+
+function App() {
+  return (
+    <div >
+      <Navibar></Navibar>
+      
+      <About></About>
+
+      <Skills></Skills>
+    </div>
+  );
+}
+
+export default App;
